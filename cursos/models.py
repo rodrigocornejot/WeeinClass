5d65105fc888ce4450443482089160ebb00f49fd
rodrigocornejot <rodrigo_gfa@hotmail.com>
2026-02-19 12:26:01 -0500

    Arreglo

--- cursos/models.py
+++ cursos/models.py
@@ -84,7 +84,7 @@ class Curso(models.Model):
 
 class Alumno(models.Model):
     nombre = models.CharField(max_length=255)
-    documento_identidad = models.CharField(
+    dni = models.CharField(
         max_length=20,
         unique=True
     )
